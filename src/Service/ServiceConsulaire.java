3b29030c545591dabcdbb8d41c7c9fe281cd13c9
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-05-12 21:32:48 -0400

    Init ServiceConsulaire java class

---
+++ src/Service/ServiceConsulaire.java
@@ -0,0 +1,11 @@
+package Service;
+
+//Pour l’obtention d’un visa, une personne s’adresse à un service consulaire qui s’occupe de délivrer un visa avec un numéro unique
+
+import Component.Personne;
+
+public class ServiceConsulaire {
+    public Visa delivrerVisa (Personne personne, String type) {
+        // Implémentation de la délivrance de visa
+    }
+}
