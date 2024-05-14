2e11e482ebb8b9d2df88bf433e89c74a128e1574
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-05-13 20:04:50 -0400

    add methods in personne class

--- src/Component/Personne.java
+++ src/Component/Personne.java
@@ -1,8 +1,8 @@
 package Component;
-//Chaque personne a :
-//o un nom,
-//o un prénom et,
-//o un passeport.
+
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Personne {
     private String nom;
     private String prenom;
@@ -13,11 +13,124 @@ public class Personne {
         this.prenom = prenom;
     }
 
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
     public Passeport getPassport() {
         return passport;
     }
 
     public void setPassport(Passeport passport) {
         this.passport = passport;
+    }
+
+    public void afficherInformations() {
+        System.out.println("Nom: " + nom);
+        System.out.println("Prenom: " + prenom);
+        if (passport != null) {
+            System.out.println("Informations du passeport : ");
+            passport.afficherInformations(); //TODO
+        } else {
+            System.out.println("Aucun passeport associé.");
+        }
+    }
+    /**
+     * Calcule et retourne l'âge de la personne en années.
+     *
+     * @return L'âge de la personne en années.
+     */
+    public int calculerAge() {
+        LocalDate aujourdhui = LocalDate.now();
+        LocalDate dateNaissance = passport.getDateNaissance(); //TODO
+        if (dateNaissance != null) {
+            return Period.between(dateNaissance, aujourdhui).getYears();
+        } else {
+            return -1; // Date de naissance inconnue
+        }
+    }
+
+    public void demanderPassport(LocalDate dateDelivrance, LocalDate dateExpiration, String lieuDelivrance) {
+        if (passport != null) {
+            Passeport nouveauPassport = new Passeport(dateDelivrance, dateExpiration, lieuDelivrance); //TODO
+            setPassport(nouveauPassport);
+            System.out.println("Passeport délivré avec succès.");
+        } else {
+            System.out.println("La personne possède déjà un passeport.");
+        }
+    }
+
+    public void demanderProlongationPasseport(LocalDate nouvelleDateExpiration) {
+        if (passport != null) {
+            passport.prolongerDateExpiration(nouvelleDateExpiration);
+            System.out.println("La date d'expiration du passeport a été prolongée avec succès.");
+        } else {
+            System.out.println("La personne n'a pas de passeport.");
+        }
+    }
+
+    public void demanderVisa(String type, LocalDate dateDelivrance, LocalDate dateExpiration) {
+        if (passport != null) {
+            Visa nouveauVisa = new Visa(type, dateDelivrance, dateExpiration); //TODO
+            passport.setVisa(nouveauVisa); //TODO
+            System.out.println("Visa délivré avec succès.");
+        } else {
+            System.out.println("La personne doit d'abord obtenir un passeport.");
+        }
+    }
+
+    public void demanderProlongationVisa(LocalDate nouvelleDateExpiration) {
+        if (passport != null && passport.getVisa() != null) { //TODO
+            passport.getVisa().prolongerDateExpiration(nouvelleDateExpiration); //TODO
+            System.out.println("La date d'expiration du visa a été prolongée avec succès.");
+        } else {
+            System.out.println("La personne n'a pas de passeport ou de visa associé.");
+        }
+    }
+
+    public void annulerPasseport() {
+        if (passport != null) {
+            passport.setValide(false); //TODO
+            passport.setVisa(null); //TODO
+            System.out.println("Le passeport a été annulé avec succès.");
+        } else {
+            System.out.println("La personne n'a pas de passeport.");
+        }
+    }
+
+    public boolean possedeVisaValide() {
+        if (passport != null && passport.getVisa() != null) { //TODO
+            return passport.getVisa().estValide(); //TODO
+        }
+        return false;
+    }
+
+    public LocalDate getDateExpirationPasseport() {
+        if (passport != null) {
+            return passport.getDateExpiration(); //TODO
+        }
+        return null;
+    }
+
+    public String getInfoVisa() {
+        if(passport != null && passport.getVisa() != null) { //TODO
+            Visa visa = passport.getVisa();
+            return "Type de visa: " + visa.getType() + "\n" +
+                    "Date de délivrance: " + visa.getDateDelivrance() + "\n" +
+                    "Date d'expiration: " + visa.getDateExpiration();
+        }
+        return "Aucun visa associé.";
     }
 }
