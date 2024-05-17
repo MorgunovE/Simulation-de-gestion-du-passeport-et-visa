a5e3ef3037ef1486d3028701e2841f35bb0c52ef
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-05-16 20:08:19 -0400

    add commentaires dans ServiceConsulaire.java

--- src/Service/ServiceConsulaire.java
+++ src/Service/ServiceConsulaire.java
@@ -7,20 +7,34 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * La classe ServiceConsulaire gère les opérations liées aux visas,
+ * y compris l'émission, la prolongation et l'annulation.
+ */
 public class ServiceConsulaire {
     private List<Visa> visas;
 
+    /**
+     * Constructeur par défaut initialisant la liste des visas.
+     */
     public ServiceConsulaire() {
         visas = new ArrayList<>();
     }
 
+    /**
+     * Méthode pour demander un visa pour une personne.
+     *
+     * @param personne La personne demandant le visa
+     * @param visa Le visa à délivrer
+     */
     public void demanderVisa(Personne personne, Visa visa) {
         if (!visas.contains(visa)) {
             visas.add(visa);
             if(personne.getPassport() == null) {
                 System.out.println("Aucun passeport associé à " + personne.getNom() + " " + personne.getPrenom());
                 return;
-            } if(visa.getDateExpiration().isAfter(personne.getPassport().getDateExpiration())){
+            }
+            if(visa.getDateExpiration().isAfter(personne.getPassport().getDateExpiration())){
                 visa.setDateExpiration(personne.getPassport().getDateExpiration());
                 personne.getPassport().setVisa(visa);
                 System.out.println("Visa délivré avec succès à " + personne.getNom() + " " + personne.getPrenom());
@@ -29,12 +43,18 @@ public class ServiceConsulaire {
                 personne.getPassport().setVisa(visa);
                 System.out.println("Visa délivré avec succès à " + personne.getNom() + " " + personne.getPrenom());
             }
-
         } else {
             System.out.println("Le visa a déjà été délivré à " + personne.getNom() + " " + personne.getPrenom());
         }
     }
 
+    /**
+     * Méthode pour demander la prolongation de la date d'expiration d'un visa.
+     *
+     * @param personne La personne demandant la prolongation
+     * @param visa Le visa à prolonger
+     * @param nouvelleDateExpiration La nouvelle date d'expiration
+     */
     public void demanderPrologationVisa(Personne personne, Visa visa, LocalDate nouvelleDateExpiration) {
         if(visas.contains(visa)) {
             if(nouvelleDateExpiration.isAfter(visa.getDateExpiration())){
@@ -52,6 +72,11 @@ public class ServiceConsulaire {
         }
     }
 
+    /**
+     * Méthode pour annuler un visa.
+     *
+     * @param visa Le visa à annuler
+     */
     public void annulerVisa(Visa visa) {
         if(visas.contains(visa)) {
             visa.setValide(false);
